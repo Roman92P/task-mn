6f68b192de5d735bc937a01984b2799d6a6487df
Roman <roman.paszkow@gmail.com>
2020-05-24 11:27:41 +0200

    11:27; 24.05.20

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,14 +1,16 @@
 package pl.coderslab;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.commons.validator.GenericValidator;
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.sql.SQLOutput;
 import java.util.*;
+
 public class TaskManager {
     public static void main(String[] args) {
 //powitanie użytkownika aplikacji + nazwa pliku, gdzie są przechowywane zadania;
@@ -38,15 +40,17 @@ public class TaskManager {
                     default:
                         System.out.println(ConsoleColors.RED + "Please select a correct option.");
                 }
+
                 if ("exit".equals(choosenButton)) {
                     System.out.print(ConsoleColors.RED_BOLD + "bye bye");
                     break;
-                }
-                System.out.println(choosenButton);
+                }System.out.println(choosenButton);
+
             }
         }
         scanner.close();
     }
+
     private static void RemoveTask(Scanner scanner, String nazwaPliku) {
         System.out.println("Enter line number");
         int toDel = checkEnterNumLine(scanner, nazwaPliku);
@@ -79,6 +83,7 @@ public class TaskManager {
             e.printStackTrace();
         }
     }
+
     private static int checkEnterNumLine(Scanner scanner, String nazwaPliku) {
         String userWrite = "";
         int max = numberOfLine(nazwaPliku);
@@ -94,21 +99,28 @@ public class TaskManager {
         result = Integer.parseInt(userWrite);
         return result;
     }
+
     private static void addTask(Scanner scanner, String nazwaPliku) {
         List<String> lines = null;
         try {
             String[] nowaLinia = new String[3];
             System.out.println("Write the description of a new task");
             String describe = "";
+            String describeCrected = "";
             while (true) {
                 describe = scanner.nextLine();
+                describeCrected = describe.replaceAll(",", "  ");
                 if (!StringUtils.isEmpty(describe)) {
                     break;
                 } else {
-                    System.out.print("This field cannot be empty. Write something");
+                    System.out.println("This field cannot be empty. Write something");
                 }
             }
-            nowaLinia[0] = numberOfLine(nazwaPliku) + " : " + describe;
+            StringBuilder builder = new StringBuilder();
+            builder.append(numberOfLine(nazwaPliku));
+            builder.append(" : ");
+            builder.append(describeCrected);
+            nowaLinia[0] = builder.toString();
             System.out.println("Write due date");
             String dt = "";
             while (true) {
@@ -116,11 +128,11 @@ public class TaskManager {
                 if (GenericValidator.isDate(dt, "yyyy-MM-dd", true)) {
                     break;
                 } else {
-                    System.out.print("Please enter Date in this format YYYY-MM-DD");
+                    System.out.println("Please enter Date in this format YYYY-MM-DD");
                 }
             }
             nowaLinia[1] = dt;
-            System.out.print("Is it important or no [put true or false]");
+            System.out.println("Is it important or no [put true or false]");
             String importance = "";
             while (scanner.hasNextLine()) {
                 importance = scanner.nextLine();
@@ -128,7 +140,7 @@ public class TaskManager {
                     nowaLinia[2] = importance;
                     break;
                 } else {
-                    System.out.print("You should enter true or false, nothing else");
+                    System.out.println("You should enter true or false, nothing else");
                 }
             }
             String newLine = Arrays.toString(nowaLinia);
@@ -150,6 +162,7 @@ public class TaskManager {
         }
         listTask(scanner, nazwaPliku);
     }
+
     private static void fileExistOrNo(String nazwaPliku) {
         if (!Files.exists(Paths.get(nazwaPliku))) {
             try {
@@ -160,6 +173,7 @@ public class TaskManager {
             }
         }
     }
+
     private static void listTask(Scanner scan, String nazwaPliku) {
         int rowc = 0;
         String[][] taskTab = new String[numberOfLine(nazwaPliku)][3];
@@ -169,21 +183,24 @@ public class TaskManager {
             while (scan.hasNextLine()) {
                 inputLine = scan.nextLine();
                 String[] rowArr = inputLine.split(",");
+                System.out.println(Arrays.toString(rowArr));
                 for ( int i = 0; i < rowArr.length; i++ ) {
                     taskTab[rowc][i] = rowArr[i];
+//                    System.out.print(taskTab[rowc][i]);
                 }
                 rowc++;
+//                System.out.println("\n");
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        for ( int i = 0; i < taskTab.length; i++ ) {
-            for ( int j = 0; j < taskTab[i].length; j++ ) {
-                System.out.print(taskTab[i][j]);
-            }
-            System.out.println("\n");
-        }
+//        for ( int i = 0; i < taskTab.length; i++ ) {
+//            for ( int j = 0; j < taskTab[i].length; j++ ) {
+//                System.out.print(taskTab[i][j]);
+//            }
+//            System.out.println("\n");
     }
+    //    }
     private static int numberOfLine(String fName) {
         Path path1 = Paths.get(fName);
         int count = 0;
@@ -196,12 +213,14 @@ public class TaskManager {
         }
         return count;
     }
+
     private static void scanTillquit(String input) {
         while (true) {
             if ("exit".equals(input))
                 break;
         }
     }
+
     private static void menuBar() {
         String[] options = {(ConsoleColors.BLUE_BOLD + "Please select option") + ConsoleColors.BLACK, "add", "remove", "list", "exit"};
         for ( String op : options ) {
